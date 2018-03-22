4fd023f2f1644e86b8b96ce8862c8ac52f3ddc2e
guppy0130 <guppy0130@yahoo.com>
2018-03-22 01:24:43 -0700

    displaying tweets works now :) and hbs implemented correctly

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const twitter = require('twit');
 const express = require('express');
 const textStatistics = require('text-statistics');
+const hbs = require('hbs');
+const reload = require('reload');
 const port = process.env.port || 5000;
 
 const client = new twitter({
@@ -18,24 +20,36 @@ const params = {
 	count: 200,
 	trim_user: true,
 	exclude_replies: false,
-	include_rts: false
+	include_rts: false,
+	tweet_mode: 'extended'
 }
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
     var cleanSentenceArr = [],
-        gradeArr = [];
+        gradeArr = [],
+		tweetGlobal = [];
 	if (error) {
 		console.log(error);
 		return
 	}
     tweets.forEach(function(element, index, array) {
-        var cleanedSentence = cleanText(element.text);
-        cleanSentenceArr.push(cleanedSentence);
+        var cleanedSentence = cleanText(element.full_text);
+		if (cleanedSentence == ' ') {
+			return;
+		}
         var gradeLevel = textStatistics(cleanedSentence).fleschKincaidGradeLevel();
-        gradeArr.push(gradeLevel > 0 ? gradeLevel : 0);
+		gradeLevel = gradeLevel > 0 ? gradeLevel : 0;
+        gradeArr.push(gradeLevel);
+		tweetGlobal.push({
+			id: element.id,
+			text: cleanedSentence,
+			gradeLevel: gradeLevel
+		});
     });
-    console.log(cleanSentenceArr);
-    console.log(gradeArr);
+	content['tweets'] = tweetGlobal;
+	return gradeArr;
+}).then(function(gradeArr) {
+	//TODO: calculate grades 'n stuff
 });
 
 function cleanText(input) {
@@ -43,9 +57,25 @@ function cleanText(input) {
           regexHash = /\S*#(?:\[[^\]]+\]|\S+)/gim,
           regexUsername = /(^|[^@\w])@(\w{1,15})\b\ /gim,
           regexHTMLGT = /\&gt\;(\ )?/gim;
-    return input.replace(regexURL, '').replace(regexHash, '').replace(regexUsername, '').replace(regexHTMLGT, '').replace(/\n/gim, '. ').trim() + '.';
-}
+    var result = input.replace(regexURL, '').replace(regexHash, '').replace(regexUsername, '').replace(regexHTMLGT, '').replace(/\n/gim, '. ').trim() + '.';
+	return result == '.' ? ' ' : result;
+};
+
+let content = {
+    title: 'Testing'
+};
+
+hbs.registerPartials(__dirname + '/views/partials', () => {
+	console.log('partials registered');
+});
+
+hbs.registerHelper('item', function(param) {
+	console.log(param);
+	return param;
+});
 
 express()
-    .get('/', (req, res) => res.send('Hello World'))
+    .set('view engine', 'hbs')
+    .set('views', './views')
+    .get('/', (req, res) => res.render('index', content))
     .listen(port, () => console.log(`Listening on ${ port }`));
