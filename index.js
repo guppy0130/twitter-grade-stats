023f111a6f17587a63bae46238d31faed9e45a10
guppy0130 <guppy0130@yahoo.com>
2018-04-08 03:32:21 -0700

    add 404 and out-of-requests (empty) pages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* jslint node:true */
 // Dependencies
 const twitter = require('twit');
 const express = require('express');
@@ -17,7 +18,7 @@ const params = {
 	exclude_replies: false,
 	include_rts: false,
 	tweet_mode: 'extended'
-}
+};
 
 function cleanText(input) {
     const regexURL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gim,
@@ -26,14 +27,13 @@ function cleanText(input) {
           regexHTMLGT = /\&gt\;(\ )?/gim;
     var result = input.replace(regexURL, '').replace(regexHash, '').replace(regexUsername, '').replace(regexHTMLGT, '').replace(/\n/gim, '. ').trim() + '.';
 	return result == '.' ? ' ' : result;
-};
+}
 
 let index = {
 	title: "Twitter Grade Stats"
 };
 
-hbs.registerPartials(__dirname + '/views/partials', () => {
-});
+hbs.registerPartials(__dirname + '/views/partials');
 
 hbs.registerHelper('item', function(param) {
 	return param;
@@ -41,25 +41,54 @@ hbs.registerHelper('item', function(param) {
 
 const getTweetMiddleware = async (req, res, next) => {
 	params['screen_name'] = req.query.username;
-	client.get('statuses/user_timeline', params)
-		.catch((err) => {})
-		.then((data, response) => {
-			return data.data.map(element => {
-				var cleanedText = cleanText(element.full_text)
-				return {
-					id: element.id,
-					text: cleanedText,
-					grade: textStatistics(cleanedText).fleschKincaidGradeLevel()
-				};
-			});
-		})
-		.then((tweets) => {
-			req.tweets = JSON.stringify(tweets);
-			next();
-		}).catch(err => {
-			req.tweets = JSON.stringify("An error occured. Are you sure you entered an appropriate handle?");
-			next();
-		});   
+	client.get('statuses/user_timeline', params).catch((err) => {
+
+    }).then((response) => {
+        console.log(req.query.username + ' resulted in a ' + response.resp.statusCode + ' response. Requests remaining: ' + response.resp.headers['x-rate-limit-remaining']);
+        if (response.resp.statusCode === 404) {
+            Promise.reject().catch(() => {
+                res.render('404', {
+                    name: req.query.username,
+                    reason: "doesn't exist"
+                });
+            });
+        } else if (response.resp.statusCode === 429) {
+            Promise.reject().catch(() => {
+                res.render('empty', {
+                    time: new Date(response.resp.headers['x-rate-limit-reset'] * 1000)
+                });
+            });
+        } else if (response.resp.statusCode === 200) {
+            return response.data.map(element => {
+                var cleanedText = cleanText(element.full_text)
+                return {
+                    id: element.id_str,
+                    text: cleanedText,
+                    grade: textStatistics(cleanedText).fleschKincaidGradeLevel()
+                };
+            });
+        } else if (response.resp.statusCode === 401) {
+            Promise.reject().catch(() => {
+                res.render('404', {
+                    name: req.query.username,
+                    reason: "was suspended"
+                })
+            });
+        }
+    }).then((tweets) => {
+        if (tweets != undefined) {
+            req.tweets = JSON.stringify(tweets);
+            next();
+        } else {
+            Promise.reject().catch(() => {});
+        }
+    }).catch(err => {
+        console.log('try not to end up here with error ' + err);
+        res.render('404', {
+            name: req.query.username,
+            reason: "caused something to break"
+        });
+    });
 };
 
 user = express.Router();
